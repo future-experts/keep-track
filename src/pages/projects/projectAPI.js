270dbecd112f8ba77ec40d53f12544a69b0ea54b
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2025-03-26 09:01:56 +0100

    axios.create added

--- src/pages/projects/projectAPI.js
+++ src/pages/projects/projectAPI.js
@@ -1,18 +1,26 @@
 import axios from "axios";
 
-const url = "http://localhost:4000/projects";
+const request = axios.create({
+  baseURL: "http://localhost:4000/",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": "token 123456"
+  }
+});
 
 export const getProjects = async () => {
-  const response = await axios.get(`${url}?_page=1&_limit=12`);
+  const response = await request.get(`projects?_page=1&_limit=12`);
   return response.data;
 }
 
 export const getProject = async id => {
-  const response = await axios.get(`${url}/${id}`);
+  const response = await request.get(`projects/${id}`);
   return response.data;
 }
 
 export const updateProject = async (data) => {
-  const response = await axios.put(`${url}/${data.id}`, data);
+  const response = await request.put(`projects/${data.id}`, data);
   return response.data;
 }
